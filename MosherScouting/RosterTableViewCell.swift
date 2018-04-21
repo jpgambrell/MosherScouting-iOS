ea95974d876e666b9f70a0ac97c473738d024c7b
jpgambrell <john.gambrell@gmail.com>
2018-04-21 16:17:41 -0500

    friday work

---
+++ MosherScouting/RosterTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RosterTableViewCell.swift
+//  MosherScouting
+//
+//  Created by Gambrell, John on 4/20/18.
+//  Copyright © 2018 Gambrell, John. All rights reserved.
+//
+
+import UIKit
+
+class RosterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
