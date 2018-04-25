a4d8f19adefc964c4fdaa696d62c6d2a03fa5cac
jpgambrell <john.gambrell@gmail.com>
2018-04-25 11:10:46 -0500

    back in layout hell

---
+++ MosherScouting/FullViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FullViewController.swift
+//  MosherScouting
+//
+//  Created by Gambrell, John on 4/25/18.
+//  Copyright © 2018 Gambrell, John. All rights reserved.
+//
+
+import UIKit
+
+class FullViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
